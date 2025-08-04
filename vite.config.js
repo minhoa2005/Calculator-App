a093a302ea9d379cee4e949db0cd5a5da13408e8
minhoa2005 <minhsoc5@gmail.com>
2025-08-04 12:59:19 +0700

    config PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'My React App',
+      short_name: 'ReactApp',
+      description: 'A sample React application with PWA support',
+      theme_color: '#ffffff',
+    }
+  })
+  ],
 })
